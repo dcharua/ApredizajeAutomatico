b34e971b17ac95a0d4e82cf138fec6cbbd7e5ff4
Daniel <danielcharua@hotmail.com>
2020-03-26 10:20:08 -0600

    carga de datos, visualizacion en tabla y JSON

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template, session, redirect
+import json
+import pandas as pd
+import numpy as np
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+data = pd.read_csv("data/incidentes-viales.csv")
+data_df1 = data['fecha_creacion']
+data_df2 = data['incidente_c4']
+df = pd.DataFrame({'fecha':data_df1,
+                'incidente':data_df2
+                })
+                
+@app.route('/', methods=("POST", "GET"))
+def data_table():
+     return render_template('index.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+@app.route('/getdata', methods=("POST", "GET"))
+def data_json():
+    return df.to_json(orient='records')
